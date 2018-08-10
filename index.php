d64aba71a0b4d8833cc3f1d3a10032b9f392d3ca
curioushoainam <hoainamtruongute@gmail.com>
2018-08-10 16:21:28 +0700

    build a very simple menu without submenu and links

--- index.php
+++ index.php
@@ -1,12 +1,44 @@
 <?php 
-function plusOne($x){
-	if($x < 10){
-		echo ++$x .'<br>';
-		plusOne($x);
-	} else {
-		echo 'finished';
-	}
+// build a very basic menu
+
+$menu = array(
+	'about' => array(
+		'display'=>'About Us'
+	),
+	'blog' => array(
+		'display'=>'Read Our Blog'
+	),
+	'links' => array(
+		'display'=>'Recommed Links'
+	),
+	'contact' => array(
+		'display'=>'Contact Us'
+	)
+);
+
+function buildMenu($menu_array){
+	$menu = '<ul>';
+	foreach($menu_array as $id => $properties) {
+		$display = $properties['display'];
+		$menu .=  '<li>'.$display.'</li>';
+	}	
+	return $menu . "</ul>";
 }
-plusOne(5);
 
 ?>
+
+<html>
+	<head></head>
+	<body>
+		<!-- <ul>
+			<li>About Us</li>
+			<li>Read Our Blog</li>
+			<li>Recommed Links</li>
+			<li>Contact Us</li>
+		</ul> -->
+		<?php 
+			echo buildMenu($menu);
+		 ?>
+	</body>
+
+</html>
